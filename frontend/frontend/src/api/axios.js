d6a486e2659095698fd806aa858fd138fe30ef92
Avirajsinghsagar <avirajsinghsagar@gmail.com>
2026-05-12 18:55:17 +0530

    fix auth token

--- frontend/frontend/src/api/axios.js
+++ frontend/frontend/src/api/axios.js
@@ -4,4 +4,24 @@ const API = axios.create({
   baseURL: "https://task-mnager-production.up.railway.app/api",
 });
 
+API.interceptors.request.use(
+  (req) => {
+
+    const userInfo = localStorage.getItem("userInfo");
+
+    if (userInfo) {
+
+      const token = JSON.parse(userInfo).token;
+
+      req.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return req;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default API;
